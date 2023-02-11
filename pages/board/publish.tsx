b3bbc24d265d01a6bc348953ab2be6024319a42e
iinsue <hinsu93@gmail.com>
2023-02-11 23:16:24 +0900

    Install React-Quill

--- pages/board/publish.tsx
+++ pages/board/publish.tsx
@@ -1,7 +1,22 @@
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+
+const QuillWrapper = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
+
 const PublishPost = () => {
+  const [value, setValue] = useState("");
   return (
     <div>
       <h1>등록페이지</h1>
+      <form>
+        <div>
+          <input type="text" />
+          <QuillWrapper theme="snow" value={value} onChange={setValue} />
+        </div>
+      </form>
     </div>
   );
 };
