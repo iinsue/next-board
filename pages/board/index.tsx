097456d5316667eedc895fde5e48e64050fbad9a
Insu Hong <hinsu93@gmail.com>
2023-02-23 15:34:27 +0900

    Update Check Process

--- pages/board/index.tsx
+++ pages/board/index.tsx
@@ -9,26 +9,64 @@ const fetchList = async () => {
 };
 
 const BoardList = () => {
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
   const [list, setList] = useState([]);
+
   const router = useRouter();
   useEffect(() => {
     fetchList().then((res) => setList(() => res.postList));
   }, []);
+
+  const handleSelectAll = (e) => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(list.map((li) => `${li.postIdx}`));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = (e) => {
+    const { id, checked } = e.target;
+    console.log(id, checked);
+    setIsCheck([...isCheck, id]);
+    if (!checked) {
+      setIsCheck(isCheck.filter((item) => item !== id));
+    }
+  };
+
+  const view = () => {
+    console.log(isCheck);
+    console.log(typeof list[0].postIdx);
+  };
 
   return (
     <div>
       <h1>글 목록</h1>
       <div>
         <Link href="/board/register">등록하기</Link>
+        <div onClick={view}>view</div>
       </div>
       {list.length > 0 ? (
         <div>
+          <input
+            type="checkbox"
+            name="selectAll"
+            id="selectAll"
+            onChange={handleSelectAll}
+            checked={isCheckAll}
+          />
           {list.map((item) => (
-            <div
-              key={item.postIdx}
-              onClick={() => router.replace(`/board/${item.postIdx}`)}
-            >
-              <div>{item.postTitle}</div>
+            <div key={item.postIdx}>
+              <input
+                type="checkbox"
+                id={item.postIdx}
+                onChange={handleClick}
+                checked={isCheck.includes(`${item.postIdx}`)}
+              />
+              <div onClick={() => router.replace(`/board/${item.postIdx}`)}>
+                {item.postTitle}
+              </div>
             </div>
           ))}
         </div>
